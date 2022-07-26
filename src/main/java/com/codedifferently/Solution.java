52ac2443c028a39654fab3a2ee2a94d2573fb4ea
Luis Adorno <luis.adorno5@upr.edu>
2022-07-26 09:08:21 -0400

    Completed problem.

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,22 @@
 package com.codedifferently;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+        Map<Character, Integer> letterCount = new TreeMap<>();
+        for(char c : input.toCharArray()){
+            letterCount.merge(c, 1, Integer::sum);
+        }
+        return formatResult(letterCount);
+    }
+
+    private String formatResult(Map<Character, Integer> map){
+        StringBuilder builder = new StringBuilder();
+        for(Map.Entry<Character, Integer> entry : map.entrySet()){
+            builder.append(entry.getKey()).append(":").append(entry.getValue()).append(" ");
+        }
+        return builder.toString().trim();
     }
 }
